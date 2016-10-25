73f424d78e0db8480b6adf1121a765896673412b
Steven Calvez <calvez.steven@gmail.com>
2016-10-25 19:14:49 +0200

    Temporary fix of the factorial function for integer too large

--- sensitivity/src/limit/bayes/stats_tools.h
+++ sensitivity/src/limit/bayes/stats_tools.h
@@ -3,8 +3,16 @@
 
 #include "TMath.h"
 
+// int factorial(int n)
+// {
+//   return std::factorial(n);
+// }
+
 int factorial(int n)
 {
+  if(n>=32)
+    return 1e9;
+
   return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
@@ -33,9 +41,10 @@ double p0_S(double S)
 
 double p0_B(double B)
 {
-  double B0 = 10;
+  double B0 = 50;
   double mu_B = B0;
-  double sigma = B0/2;
+  double sigma = 7;
+  // double sigma = B0/2;
   return B >=0 ? TMath::Gaus(B,mu_B,sigma,true) : 0;
 }
 
@@ -44,10 +53,30 @@ double proba_bayes(double S, double B,
 
   double p = 1;
 
+  // std::cout << "PROBA BAYES" << std::endl;
+
   for(unsigned int i = 0; i<pdf_S.size(); ++i) {
     double lambda_i = S*pdf_S[i] + B*pdf_B[i];
+    // std::cout << "------- " << i << std::endl;
+
+    // std::cout << "lambda " << lambda_i << std::endl;
+
+    // std::cout << "pow(lambda_i,data[i]) " << pow(lambda_i,data[i]) << std::endl;
+    // std::cout << "exp(-lambda_i)/factorial(int(data[i])) " << exp(-lambda_i)/factorial(int(data[i])) << std::endl;
+
     p *= pow(lambda_i,data[i])*exp(-lambda_i)/factorial(int(data[i]));
+
+    // std::cout << "p " << p << std::endl;
+
+    if(std::isinf(p) || p<1e-300)
+      return 0;
+
+    // if(S<=1 && B<=1)
+    //   std::cout << "p " << p << std::endl;
+
+    // std::cout << "--  i proba bayes "<< i << "  " << pow(lambda_i,data[i])*exp(-lambda_i)/factorial(int(data[i])) << std::endl;
   }
+
 
   return p;
 }
