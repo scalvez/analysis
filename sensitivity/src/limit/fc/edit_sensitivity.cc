e3d13dd43409d557d3c969cbdcc6aba6be29b588
Steven Calvez <calvez.steven@gmail.com>
2016-11-16 22:28:49 +0100

    Added plot for background free hypothesis

--- sensitivity/src/limit/fc/edit_sensitivity.cc
+++ sensitivity/src/limit/fc/edit_sensitivity.cc
@@ -11,6 +11,7 @@ void edit_sensitivity() {
 
   TFile *f = TFile::Open("sensitivity.root");
 
+  TGraph *g_0 = (TGraph*)f->Get("hl_radon_exp_0");
   TGraph *g_50 = (TGraph*)f->Get("hl_radon_exp_50");
   TGraph *g_150 = (TGraph*)f->Get("hl_radon_exp_150");
   TGraph *g_500 = (TGraph*)f->Get("hl_radon_exp_500");
@@ -18,19 +19,26 @@ void edit_sensitivity() {
   Double_t xl1=.65, yl1=0.7, xl2=0.9, yl2=0.9;
   TLegend *leg = new TLegend(xl1,yl1,xl2,yl2);
   leg->SetHeader("Radon contamination :");
+  leg->AddEntry(g_0,"No radon");
   leg->AddEntry(g_50,"50 #muBq/m^{3}");
   leg->AddEntry(g_150,"150 #muBq/m^{3}");
   leg->AddEntry(g_500,"500 #muBq/m^{3}");
   leg->SetFillColor(kWhite);
+  // gStyle->SetPadTicksy(1) ;
 
+  g_0->SetFillColor(kWhite);
   g_50->SetFillColor(kWhite);
   g_150->SetFillColor(kWhite);
   g_500->SetFillColor(kWhite);
 
-  g_50->Draw("");
+  // gStyle->SetPadTicksy(1)
+
+  g_0->Draw("");
+  g_50->Draw("same");
   g_150->Draw("same");
   g_500->Draw("same");
   leg->Draw("same");
+
 
   return;
 }
